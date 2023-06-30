b870d1b6e60010af371fdf20ae06e328ebb2a57e
mosakrm <mosakrm001@gmail.com>
2023-06-30 15:55:03 +0300

    Version 1.0

---
+++ QuizGame.py
@@ -0,0 +1,60 @@
+Points = 0
+
+print("Welcome To The Quiz Game !!")
+
+playing = input("Do you want to play? > ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's Play :)")
+
+
+# Question 1
+
+answer = input("What is the orange part of an egg called ? > ")
+
+if answer.lower() == "yolk":
+    print("Correct !")
+    Points += 1
+else:
+    print("Incorrect !")
+
+#Question 2
+
+answer = input("What is the closest planet to the Sun ? > ")
+
+if answer.lower() == "mercury":
+    print("Correct !")
+    Points += 1
+else:
+    print("Incorrect !")
+
+#Question 3
+
+answer = input("How many days are there in a year ? > ")
+
+if answer.lower() == "365":
+    print("Correct !")
+    Points += 1
+else:
+    print("Incorrect !")
+
+
+#Question 4
+
+answer = input("In which country can you find the Eiffel Tower ? > ")
+
+if answer.lower() == "france":
+    print("Correct !")
+    Points += 1
+else:
+    print("Incorrect !")
+
+
+print("""
+
+""")
+print(f"     Game Is Over ")
+print(f"    You Got {Points} Correct Answers out of 4")
+
